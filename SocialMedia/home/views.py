093d1774b271e947f232a41ac4815fc55f4542c5
aliyaghini <yaghini.a110@gmail.com>
2023-05-09 07:50:38 -0700

    add initial templates

--- SocialMedia/home/views.py
+++ SocialMedia/home/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse('Home page ...')
+    return render(request, 'home/index.html')
